b2834f13eaa14239a17e461e57fb1026fd991684
catcherochek <catcherochek@yandex.ru>
2020-07-19 10:52:24 +0300

    1st_commit

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://tovarydladoma.test/admin/');
+define('HTTP_CATALOG', 'http://tovarydladoma.test/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://tovarydladoma.test/admin/');
+define('HTTPS_CATALOG', 'http://tovarydladoma.test/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/laragon/www/tovarydladoma/admin/');
+define('DIR_SYSTEM', 'C:/laragon/www/tovarydladoma/system/');
+define('DIR_IMAGE', 'C:/laragon/www/tovarydladoma/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', 'C:/laragon/www/tovarydladoma/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'tovarydladoma');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
